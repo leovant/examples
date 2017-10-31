b0b0e72bb5b2a71a346222332efd3d605a470630
Leo Tavares <leovant@gmail.com>
2017-10-30 22:01:15 -0200

    Implement Reddit clone using Firebase.

---
+++ react/reddit-clone/src/containers/App/firebase-config.js
@@ -0,0 +1,8 @@
+export default {
+  apiKey: 'AIzaSyAjr2WT0MGg1DtdOQdCBd2pO9SyDjBgr3o',
+  authDomain: 'reddit-clone-74e7d.firebaseapp.com',
+  databaseURL: 'https://reddit-clone-74e7d.firebaseio.com',
+  projectId: 'reddit-clone-74e7d',
+  storageBucket: 'reddit-clone-74e7d.appspot.com',
+  messagingSenderId: '737267146131'
+};
---
+++ react/reddit-clone/src/containers/Posts/index.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+
+class Posts extends Component {
+  handleUpvote = (post, key) => {
+    this.props.firebase.ref('posts/' + key).set({
+      title: post.title,
+      upvote: post.upvote + 1,
+      downvote: post.downvote
+    });
+  };
+
+  handleDownvote = (post, key) => {
+    this.props.firebase.ref('posts/' + key).set({
+      title: post.title,
+      upvote: post.upvote,
+      downvote: post.downvote + 1
+    });
+  };
+
+  render() {
+    let posts = this.props.posts;
+    let _this = this;
+
+    if (!posts) {
+      return false;
+    }
+
+    if (this.props.loading) {
+      return <div>Loading</div>;
+    }
+
+    return (
+      <div className="Posts">
+        {Object.keys(posts).map(function(key) {
+          return (
+            <div key={key}>
+              <div>Title: {posts[key].title}</div>
+              <div>Upvotes: {posts[key].upvote}</div>
+              <div>Downvotes: {posts[key].downvote}</div>
+              <div>
+                <button
+                  type="button"
+                  onClick={_this.handleUpvote.bind(this, posts[key], key)}
+                >
+                  Upvote
+                </button>
+                <button
+                  type="button"
+                  onClick={_this.handleDownvote.bind(this, posts[key], key)}
+                >
+                  Downvote
+                </button>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
+}
+
+export default Posts;
